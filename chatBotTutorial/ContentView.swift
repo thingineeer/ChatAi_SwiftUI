4d1e4f1d7ef97c0f8ac8ce76d47e7fb099c005de
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-04-29 21:08:51 +0900

    Initial Commit

---
+++ chatBotTutorial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  chatBotTutorial
+//
+//  Created by 이명진 on 2023/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chatBotTutorial/chatBotTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatBotTutorialApp.swift
+//  chatBotTutorial
+//
+//  Created by 이명진 on 2023/04/29.
+//
+
+import SwiftUI
+
+@main
+struct chatBotTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
