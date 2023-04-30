f3c3f89cba519817056c3253370e0f6f8b9430eb
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-04-30 22:05:24 +0900

    feat: UPDATE ContentView

---
+++ chatBotTutorial/BotResponse.swift
@@ -0,0 +1,22 @@
+//
+//  BotResponse.swift
+//  chatBotTutorial
+//
+//  Created by 이명진 on 2023/04/29.
+//
+
+import Foundation
+
+func getBotResponse(message: String) -> String {
+    let tempMessage = message.lowercased()
+    
+    if tempMessage.contains("hello"){
+        return "Hey there!"
+    } else if tempMessage.contains("goodbye"){
+        return "Talk to you later!"
+    } else if tempMessage.contains("how are you") {
+        return "I'm fine, how about you?"
+    } else {
+        return "That's cool."
+    }
+}
